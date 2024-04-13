1abd2b1432b5a0df07d38f03e4767b859a92bc37
zorobests <166844400+zorobests@users.noreply.github.com>
2024-04-13 18:06:23 +0530

    Update funcn.py

--- bot/funcn.py
+++ bot/funcn.py
@@ -98,23 +98,6 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         else:
             await event.edit("{}\n\n{}".format(type_of_ps, tmp))
 
-
-async def test(event):
-    try:
-        zylern = "speedtest --simple"
-        fetch = await asyncrunapp(
-            zylern,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await fetch.communicate()
-        result = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
-        await event.reply("**" + result + "**")
-    except FileNotFoundError:
-        await event.reply("**Install speedtest-cli**")
-
-
 async def sysinfo(e):
     if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
         return
